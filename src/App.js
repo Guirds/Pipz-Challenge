06cfba3f30c4fc310bb1cffa32fe20c7569c13c4
Guilherme Santos <gui07015@gmail.com>
2019-05-24 22:37:47 -0300

    Add GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import './styles/global';
+import GlobalStyle from './styles/global';
 
 import Main from './pages/Main';
 
 const App = () => (
   <>
+    <GlobalStyle />
     <Main />
   </>
 );
