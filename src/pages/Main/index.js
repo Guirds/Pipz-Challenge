ef6b012144ea4c1cedc8fcf776b0c6dbedbe54ed
Guilherme Santos <gui07015@gmail.com>
2019-05-24 22:41:50 -0300

    State created

---
+++ src/pages/Main/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+import { Container, Form } from './styles';
+import FilmList from '../../components/FilmList';
+
+export default class Main extends Component {
+  state = {
+    filmInput: '',
+    films: [],
+  };
+
+  handleAddFilm = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.get(`/${this.state.filmInput}/?format=json`);
+
+      this.setState({
+        filmInput: '',
+        films: [...this.state.films, response.data],
+      });
+    } catch (err) {}
+  };
+
+  render() {
+    return (
+      <>
+        <Container>
+          <h1>Pipz's Challenge</h1>
+          <Form onSubmit={this.handleAddFilm}>
+            <input
+              type="text"
+              placeholder="Digite o número do filme ex:1"
+              value={this.state.filmInput}
+              onChange={e => this.setState({ filmInput: e.target.value })}
+            />
+            <button type="submit">Ok</button>
+          </Form>
+
+          <FilmList films={this.state.films} />
+        </Container>
+      </>
+    );
+  }
+}
